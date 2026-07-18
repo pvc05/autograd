2f66db62aadebd88cf66f018d1653c65bb837b72
Pranav C <xtreme5351@gmail.com>
2026-07-18 12:25:41 +0100

    [CLN] More codebase cleanup

--- src/ops/multiplication.cpp
+++ src/ops/multiplication.cpp
@@ -1,25 +1,10 @@
 //
-// Created by Pranav C on 29/12/2025.
+// Created by Pranav C on 18/07/2026.
 //
 
 #include "../include/operations.h"
 
-#include <stdexcept>
-
 namespace autograd {
-
-TensorOne operator-(const TensorOne& a, const TensorOne& b) {
-  if (a.tape != b.tape) {
-    throw std::invalid_argument("Tensors must have the same tape");
-  }
-  if (a.tape == nullptr) {
-    std::vector<double> res(a.size());
-    for (size_t i = 0; i < a.size(); ++i) res[i] = a.data[i] - b.data[i];
-    return TensorOne(res, false, nullptr);
-  }
-  const size_t new_id = a.tape->sub_one(a.node_id, b.node_id);
-  return TensorOne(a.tape, new_id);
-}
 
 TensorOne operator*(const TensorOne& a, const TensorOne& b) {
   if (a.tape != b.tape) {
--- src/ops/subtraction.cpp
+++ src/ops/subtraction.cpp
@@ -1,3 +1,22 @@
 //
 // Created by Pranav C on 18/07/2026.
 //
+
+#include "../include/operations.h"
+
+namespace autograd {
+
+TensorOne operator-(const TensorOne& a, const TensorOne& b) {
+  if (a.tape != b.tape) {
+    throw std::invalid_argument("Tensors must have the same tape");
+  }
+  if (a.tape == nullptr) {
+    std::vector<double> res(a.size());
+    for (size_t i = 0; i < a.size(); ++i) res[i] = a.data[i] - b.data[i];
+    return TensorOne(res, false, nullptr);
+  }
+  const size_t new_id = a.tape->sub_one(a.node_id, b.node_id);
+  return TensorOne(a.tape, new_id);
+}
+
+}  // namespace autograd
